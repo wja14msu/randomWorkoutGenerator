e54a8a8b93c0ca3f43b4490a463bdc377b7e685d
Wander5 <wja14msu@gmail.com>
2023-09-14 16:58:46 -0500

    work on displaying pictures

--- randomWorkoutGenerator.js
+++ randomWorkoutGenerator.js
@@ -1,11 +1,40 @@
-const armWorkouts = ['PUSH UPS!', 'CURLS!', 'SKY PRESS!', 'OVERHEAD TRICEPS', 'SHOULDER FLY!', 'BOX!', 'BENT-OVER ROW!'];
-const legWorkouts = ['SQUATS!', 'LUNGES!', 'CALF RAISES!', 'SIDE LUNGES!', 'HEAD KICKS!', 'FRONT-CHOPPING WOOD', 'HIGH KNEES!'];
-const coreWorkouts = ['CRUNCHES', 'PLANK!', 'SIDE-CHOPPING WOOD!', 'FRONT-CHOPPING WOOD', 'FLUTTER KICKS', 'SITTING TWISTS', 'STANDING KNEE TO ELBOW!', 'PLANK CRUNCHES!', 'BICYCLE CRUNCHES'];
+const armWorkouts = ['ARM CHOPS', ' ARM CHOPS 2', 'BACKFISTS', 'CLOSE GRIP PUSH UPS', 'HOOKS', 'OVERHEAD', 'PIKE PUSH UPS', 'PUNCHES', 'PUSH UPS', 'RAISED LEG PUSH UPS', 'TRICEP EXTENSIONS', 'UPPERCUTS'];
+const legWorkouts = ['BRIDGES', 'BUTT KICKS', 'CALF RAISES', 'DONKEY KICKS', 'FRONT KICKS', 'HIGH KNEES', 'HOOK KICKS', 'JUMPING LUNGES', 'JUMP KNEE TUCKS', 'JUMP SQUATS', 'KNEE STRIKES', 'LUNGES', 'LUNGE STEP UPS', 'MARCH STEPS', 'SHRIMP SQUATS', 'SIDE KICKS', 'SIDE LEG RAISES', 'SIDE TO SIDE LUNGES', 'SINGLE LEG BRIDGES', 'SQUAT HOLD PUNCHES', 'SQUATS', 'WALL SIT'];
+const coreWorkouts = ['CLIMBERS', 'CLIMBER TAPS', 'CROSS CRUNCHES', 'CRUNCHES', 'ELBOW PLANK', 'L SIT', 'FLUTTER KICKS', 'HOLLOW HOLD', 'KNEE CRUNCHES', 'KNEE TO ELBOW CRUNCHES', 'KNEE TO ELBOWS', 'LEG RAISES', 'PLANK JACKS', 'PLANK JUMP INS', 'PLANK LEG RAISES', 'PLANK ROTATIONS', 'PLANK WALK OUTS', 'RAISED LEG HOLD', 'REVERSE CRUNCHES', 'SHOULDER TAPS', 'SIDE BRIDGES', 'SIDE ELBOW PLANK', 'SITTING TWISTS', 'SIT UPS', 'SLOW CLIMBERS', 'V UPS'];
 const allWorkouts = ['ARM CHOPS', 'ARM CHOPS 2', 'BACKFISTS', 'BASIC BURPEES', 'BODY SAW', 'BRIDGES', 'BURPEES', 'BUTT KICKS', 'CALF RAISES', 'CLIMBERS', 'CLIMBER TAPS', 'CLOSE GRIP PUSH UPS', 'CROS CRUNCHES', 'CRUNCHES', 'DONKEY KICKS', 'ELBOW PLANK', 'ELL SIT', 'FLUTTER KICKS', 'FRONT KICKS', 'GET UPS', 'HIGH KNEES', 'HOLLOW HOLD', 'HOOK KICKS', 'HOOKS', 'JUMPING JACKS', 'JUMPING LUNGES', 'JUMPING TEES', 'JUMP KNEE TUCKS', 'JUMP SQUATS', 'KNEED CRUNCHES', 'KNEE STRIKES', 'KNEE TO ELBOW CRUNCHES', 'KNEE TO ELBOWS', 'LEG RAISES', 'LUNGES', 'LUNGE STEUP UPS', 'MARCH STEPS', 'OVERHEAD PUNCHES', 'PIKE PUSH UPS', 'PLANK JACKS', 'PLANK JUMP INS', 'PLANK LEG RAISES', 'PLANK ROTATIONS', 'PLANK WALK OUTS', 'PUNCHES', 'PUSH UPS', 'RAISED LEG HOLD', 'RAISED LEG PUSH UPS', 'REVERSE CRUNCHES', 'SEAL JACKS', 'SHOULDER TAPS', 'SHRIMP SQUATS', 'SIDE BRIDGES', 'SIDE ELBOW PLANK', 'SIDE KICKS', 'SIDE LEG RAISES', 'SIDE TO SIDE LUNGES', 'SINGLE LEG BRIDGES', 'SITTING TWISTS', 'SIT UPS', 'SLOW CLIMBERS', 'SPLIT JACKS', 'SQUAT HOLD PUNCHES', 'SQUATS', 'TRICEP EXTENSIONS', 'UPPERCUTS', 'V UPS', 'WALL SIT'];
 
 const timerDisplay = document.getElementById('timerDisplay');
 const roundCountDisplay = document.getElementById('roundCountDisplay');
 const workoutDisplay = document.getElementById('workout-display');
+
+const modifiedArray = armWorkouts.map((item) => {
+    // Replace spaces with hyphens
+    const hyphenatedItem = item.replace(/ /g, '-');
+
+    // Add file extension (e.g., ".txt" for the first item)
+    const fileExtension = item.split('.').pop(); // Extract the file extension)
+    const newItem = hyphenatedItem.replace(`.${fileExtension}`, '') + '.png';
+
+    return newItem;
+});
+
+//console.log("modifiedArray: ", modifiedArray);
+
+
+
+function hyphenSpace(workout) {
+    // Replace spaces with hyphens
+    const modifiedString = workout.replace(/ /g, '-');
+
+    // Add the file extension
+    const workoutFileName = modifiedString.toLowerCase() + '.png';
+    console.log("workoutFileName", workoutFileName);
+    
+
+    return workoutFileName
+}
+
+
 
 function updateTime() {
     const timeContainer = document.getElementById("time-container");
@@ -28,7 +57,19 @@ setInterval(updateTime, 1000);
 
 function generateWorkout(bodySection) {
     const randomIndex = Math.floor(Math.random() * bodySection.length);
-    workoutDisplay.textContent = bodySection[randomIndex];
+    let workout = bodySection[randomIndex];
+    workoutDisplay.textContent = workout;
+
+    console.log("workout: ", workout);
+    let workoutPictureContainer = document.getElementById('workout-picture');
+    //workoutPicture = hyphenSpace(workout);
+    //console.log("workoutFileName: ", workoutFileName);
+    //workoutPicture = workout;
+
+    //console.log("workoutPicture", workoutPicture);
+    image = document.getElementById("workout-picture")
+    workoutPictureContainer.src = hyphenSpace(workout);
+    return workoutFileName;
 }
 
 function formatTime(seconds) {
